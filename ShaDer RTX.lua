bfcbf7e82a644f0357b48da7dfcc3c3edec730fe
Fian_gg <fiangaming005@gmail.com>
2025-02-18 13:08:04 +0700

    ShaDer RTX.lua

--- ShaDer RTX.lua
+++ ShaDer RTX.lua
@@ -2,8 +2,8 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/blood
 Library.DefaultColor = Color3.fromRGB(255,0,0)
 
 Library:Notification({
-	Text = "Loading...",
-	Duration = 10
+	Text = "Loading... (10 second)",
+	Duration = 5
 })
 
 wait (10)
